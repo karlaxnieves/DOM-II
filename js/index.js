3a8a349f15a55fa1d4e0655d7c82a96f1933510a
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-03-09 15:27:13 -0800

    changed the background color

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const nav = document.querySelectorAll('.nav-link');
+Array.from(nav).forEach(link => {
+    link.addEventListener('mouseover', (event) => {
+        event.target.style.color = 'orange';
+        setTimeout(function () {
+            event.target.style.color = "black";
+        }, 200);
+    });
+});
+
+document.addEventListener('click', (event) => { event.target.style.backgroundColor = 'pink'; });
+
+
+const scroll = document.querySelectorAll('desination');
+
