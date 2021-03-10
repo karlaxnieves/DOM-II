5b795ccb52059a9a3a924f9c093a4c68ef08a364
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-03-09 20:06:45 -0800

    added dbclick

--- js/index.js
+++ js/index.js
@@ -9,12 +9,10 @@ Array.from(nav).forEach(link => {
     });
 });
 
-document.addEventListener('click', (event) => { event.target.style.backgroundColor = 'pink'; });
-
 
 const cta = document.querySelectorAll('.btn');
 Array.from(cta).forEach(link => {
-    link.addEventListener('mouseover', (event) => {
+    link.addEventListener('click', (event) => {
         event.target.style.backgroundColor = 'green';
         setTimeout(function () {
             event.target.style.color = "yellow";
@@ -22,3 +20,15 @@ Array.from(cta).forEach(link => {
     });
 });
 
+const mainNav = document.querySelector('.main-navigation');
+mainNav.style.borderBottom = 'solid 5px black';
+mainNav.style.backgroundColor = 'gold';
+
+const backGround = document.querySelector('.home');
+backGround.style.backgroundColor = 'pink';
+
+cta.addEventListener('dbclick', runEvent);
+
+function runEvent(e) {
+    console.log('EVENT TYPER: ' + e.type);
+}
