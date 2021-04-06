7fcc12a0308b05477869b7c335b484c5786792f5
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-04-06 16:03:20 -0700

    working on wheel event

--- js/index.js
+++ js/index.js
@@ -10,19 +10,43 @@ Array.from(nav).forEach(link => {
 });
 
 
+const cta = document.querySelectorAll('.btn');
+Array.from(cta).forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.target.style.backgroundColor = 'pink';
+        setTimeout(function () {
+            event.target.style.color = "aqua";
+        });
+    });
+});
+
+const backGround = document.querySelector('.home');
+backGround.style.backgroundColor = 'orange';
+
+
+const mainNav = document.querySelector('.main-navigation');
+mainNav.style.borderBottom = 'solid 5px black';
+mainNav.style.backgroundColor = 'gold';
+
 function zoom(event) {
     event.preventDefault();
 
-    scale += event.deltaY * -0.01;
+    if (event.deltaY < 0) {
 
-    scale = Math.min(Math.max(.125, scale), 4);
+        scale *= event.deltaY * -2;
+    }
+    else {
+
+        scale /= event.deltaY * 2;
+    }
+
+
+    scale = Math.min(Math.max(.125, scale), 1);
+
 
     el.style.transform = `scale(${scale})`;
 }
 
 let scale = 1;
-const el = document.querySelector('div')
-el.onwheel = zoom;
-
-
-
+const el = document.querySelector('img');
+document.onwheel = zoom;
