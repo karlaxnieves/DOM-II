0871c136dd065a2e34f8b5dc5443727e3e0dca6c
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-04-06 14:03:22 -0700

    styled the nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+const nav = document.querySelectorAll('nav a');
+Array.from(nav).forEach(link => {
+    link.addEventListener('mouseover', (event) => {
+        event.target.style.color = 'green';
+        setTimeout(function () {
+            event.target.style.color = "black";
+        }, 200);
+    });
+});
+
+
+function zoom(event) {
+    event.preventDefault();
+
+    scale += event.deltaY * -0.01;
+
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    el.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const el = document.querySelector('div')
+el.onwheel = zoom;
+
+
+
